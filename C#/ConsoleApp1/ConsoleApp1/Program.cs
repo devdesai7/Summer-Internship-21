b2c0d45113d37c49c10efc4ef7bbb50f63a1047e
devdesai7 <dev230199@yahoo.in>
2021-05-26 02:08:20 +0530

    First Commit

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+
+
+   
+}
