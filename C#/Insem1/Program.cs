87c47eb5cd9c43ba02c129997a9fb78180797f86
devdesai7 <dev230199@yahoo.in>
2021-05-28 15:29:41 +0530

    Task2 Insem1

---
+++ C#/Insem1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Insem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Q1();
+            //  Q2();
+            Q3();
+        }
+
+        public static void Q3()
+        {
+            try
+            {
+               int  a = int.Parse(Console.ReadLine());
+               int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(a + b);
+            }
+            catch (OverflowException Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+        }
+
+
+        public static void Q2()
+        {
+            string s = "hello world";
+            s=s.Replace('o', 'i');
+           s=s.Replace("r", string.Empty);
+            Console.WriteLine(s); // Helli wild
+
+        }
+
+
+        public static void Q1()
+        {
+
+            {
+                int a = 10, b = 20;
+                int sum = Calculator.AddNums(a, b);
+                string output = string.Format("The sum {0} and {1} is {2}", a, b,sum);
+                Console.WriteLine(output);
+
+            }
+
+        }
+    }
+
+    class Calculator
+    { 
+            public static int AddNums(int a , int b )
+              {
+                return a + b;
+             }
+
+    
+    }
+
+}
